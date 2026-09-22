13043741cd8d8eac2827495f67e64705a40b4200
shirookuroo1210-afk <shirookuroo1210@gmail.com>
2026-09-22 22:05:17 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));
+    int angka_rahasia = rand() % 100 +1;
+    int tebakan;
+
+    while(1)
+    {
+        printf("tebak angka antara 1-100: ");
+        scanf("%d", &tebakan);
+
+        if(tebakan == angka_rahasia)
+        {
+            printf("tebakan benar,angkanya adalah %d\n", angka_rahasia);
+            break;
+        }
+        else if(tebakan < angka_rahasia)
+        {
+            printf("angka terlalu kecil\n");
+        }
+        else
+        {
+            printf("angka terlalu besar\n");
+        }
+    }
+    return 0;
+}
---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+angka_rahasia = random.randint(1, 100)  
+tebakan = 0
+
+print("Tebak angka 1-100!")
+
+while True:   
+    tebakan = int(input("Masukkan tebakan: "))   
+    
+    if tebakan == angka_rahasia:
+        print(f"Benar, Angkanya {angka_rahasia}")
+        break
+    elif tebakan < angka_rahasia:
+        print("angka terlalu kecil")   
+    else:
+        print("angka terlalu besar")   
